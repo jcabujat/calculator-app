5d25f37f8af3b34e36e5b9314215ac99001bb255
Jonathan Cabujat <jonathancabujat@gmail.com>
2020-05-01 18:53:28 +0800

    Finalized Calculator App - mapped all operation buttons and calculation logic - fixed landscape layout (removed title bar when in landscape) - override onSaveInstanceState and onRestoreInstanceState methods to save interface values - added button interface and logic for Del(Delete), CE(Clear Entry), AC(All Clear) and Neg(Negate)

--- app/src/main/java/com/jocabujat/calculatorapp/MainActivity.java
+++ app/src/main/java/com/jocabujat/calculatorapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jocabujat.calculatorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,24 +11,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Variables for the display interface
     private EditText resultNum;
     private EditText inputNum;
     private TextView displayOperation;
 
     // Variables to hold operands and type of calculation
     private Double operand1 = null;
-    private Double operand2 = null;
     private String pendingOperation = "=";
+
+    //Keys used for restore instance state
+    private static final String STATE_PENDING_OPERATION = "PendingOperation";
+    private static final String STATE_OPERAND1 = "Operand1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Removing title bar when in landscape so layout will still work
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getSupportActionBar().hide();
+        }
+
+        // Assign layout fields to variables
         resultNum = findViewById(R.id.resultNum);
         inputNum = findViewById(R.id.inputNum);
         displayOperation = findViewById(R.id.operation);
 
+        // Set Number buttons
         Button button0 = findViewById(R.id.button0);
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
@@ -39,11 +52,6 @@ public class MainActivity extends AppCompatActivity {
         Button button9 = findViewById(R.id.button9);
         Button buttonDot = findViewById(R.id.buttonDot);
 
-        Button buttonEquals = findViewById(R.id.buttonEquals);
-        Button buttonDivide = findViewById(R.id.buttonDivide);
-        Button buttonMultiply = findViewById(R.id.buttonMultiply);
-        Button buttonMinus = findViewById(R.id.buttonMinus);
-        Button buttonPlus = findViewById(R.id.buttonPlus);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,6 +71,138 @@ public class MainActivity extends AppCompatActivity {
         button9.setOnClickListener(listener);
         buttonDot.setOnClickListener(listener);
 
+        // Set Operation buttons
+        Button buttonEquals = findViewById(R.id.buttonEquals);
+        Button buttonDivide = findViewById(R.id.buttonDivide);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
 
+        View.OnClickListener opListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                String op = b.getText().toString();
+                String value = inputNum.getText().toString();
+                try {
+                    Double doubleValue = Double.valueOf(value);
+                    performOperation(doubleValue, op);
+                } catch (NumberFormatException e) {
+                    inputNum.setText("");
+                }
+                pendingOperation = op;
+                displayOperation.setText(pendingOperation);
+            }
+        };
+        buttonPlus.setOnClickListener(opListener);
+        buttonMinus.setOnClickListener(opListener);
+        buttonMultiply.setOnClickListener(opListener);
+        buttonDivide.setOnClickListener(opListener);
+        buttonEquals.setOnClickListener(opListener);
+
+        // Set Negate button
+        Button buttonNeg = findViewById(R.id.buttonNeg);
+        buttonNeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = inputNum.getText().toString();
+                if (value.isEmpty()) {
+                    inputNum.setText("-");
+                } else {
+                    try {
+                        Double doubleValue = Double.valueOf(value);
+                        doubleValue *= -1;
+                        inputNum.setText(doubleValue.toString());
+                    } catch (NumberFormatException e) {
+                        inputNum.setText("");
+                    }
+                }
+            }
+        });
+
+        // Set CE (Clear Entry) button
+        Button buttonCE = findViewById(R.id.buttonCE);
+        buttonCE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputNum.setText("");
+            }
+        });
+
+        // Set AC (All Clear) button
+        Button buttonAC = findViewById(R.id.buttonAC);
+        buttonAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputNum.setText("");
+                resultNum.setText("");
+                operand1 = null;
+                pendingOperation = "=";
+                displayOperation.setText(pendingOperation);
+
+            }
+        });
+
+        // Set Delete button
+        Button buttonDel = findViewById(R.id.buttonDel);
+        buttonDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = inputNum.getText().toString();
+                String newInput = null;
+                if ((input != null) && (input.length() > 0)) {
+                    newInput = input.substring(0, input.length() - 1);
+                }
+                inputNum.setText(newInput);
+            }
+        });
+
+    }
+
+    private void performOperation(Double value, String operation) {
+        if (operand1 == null) {
+            operand1 = value;
+        } else {
+            switch (pendingOperation) {
+                case "=":
+                    operand1 = value;
+                    break;
+                case "/":
+                    if (value == 0) {
+                        operand1 = 0.0;
+                    } else {
+                        operand1 /= value;
+                    }
+                    break;
+                case "*":
+                    operand1 *= value;
+                    break;
+                case "-":
+                    operand1 -= value;
+                    break;
+                case "+":
+                    operand1 += value;
+                    break;
+            }
+        }
+        resultNum.setText(operand1.toString());
+        inputNum.setText("");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation);
+        if (operand1 != null) {
+            outState.putDouble(STATE_OPERAND1, operand1);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
+        operand1 = savedInstanceState.getDouble(STATE_OPERAND1);
+        displayOperation.setText(pendingOperation);
     }
 }
